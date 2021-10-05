dfe9671b72c51bad0c66b56d72f62da041ff2fef
Soumili1818 <74952491+Soumili1818@users.noreply.github.com>
2021-10-05 22:16:35 +0530

    Create task4.cpp

---
+++ PRACTICAL-3/task4.cpp
@@ -0,0 +1,12 @@
+//How to resolve above issue? [Hint: by using, getline(cin, line);]
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    char ch[100];
+    cin.getline(ch,100);
+    cout<<"The value stored inside ch is = "<<ch<<"\n";
+    return 0;
+}
