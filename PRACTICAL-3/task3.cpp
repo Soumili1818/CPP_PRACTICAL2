28ccdd92458046d1fad5f6ce0de10bc02c122f67
Soumili1818 <74952491+Soumili1818@users.noreply.github.com>
2021-10-05 22:14:21 +0530

    Create task3.cpp

---
+++ PRACTICAL-3/task3.cpp
@@ -0,0 +1,12 @@
+//Take a char variable and use cin to take its value from the user, cout it and it will only return one character and loss rest of the data you have entered!
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    char ch;
+    cin>>ch;
+    cout<<"The value stored inside ch is = "<<ch<<"\n";
+    return 0;
+}
